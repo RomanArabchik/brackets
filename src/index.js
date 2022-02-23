62b13d1d807765cddbee0fbad4840230b4f1b2a0
Roman Arabchik <roma_x24@mail.ru>
2022-02-24 02:53:29 +0300

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const openBrackets = ['(', '{', '[', '1', '3', '5']
+  const bracketsPair = {
+    [')']: '(',
+    ['}']: '{',
+    [']']: '[',
+    ['2']: '1',
+    ['4']: '3',
+    ['6']: '5'
+  }
+
+  const stack = []
+
+  for(let i = 0; i < str.length; i++) {
+    let current = str[i]
+
+    if(openBrackets.includes(current) || (current === '7' || current === '8' || current === '|') && !stack.includes(current)) {
+      stack.push(current)
+    } else {
+      if (stack.length === 0) return false
+
+      let top = stack[stack.length - 1]
+
+      if(bracketsPair[current] === top || (current === '7' && top === '7' || current === '8' && top === '8' || current === '|' && top === '|')) {
+        stack.pop()
+      } else {
+        return  false
+      }
+    }
+  }
+
+  return stack.length === 0
 }
